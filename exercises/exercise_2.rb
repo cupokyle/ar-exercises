2379471412e1c1c050deabab9a491930a1989a5d
cupokyle <kyleforsythcomedy@gmail.com>
2021-09-13 22:50:31 +0000

    Exercise 2 complete

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,10 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+# Change name of Store 1 to Christian
+@store1.update(name: "Christian")
+
