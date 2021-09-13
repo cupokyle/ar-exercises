e4bf12866b40503f578464f266b6db5bd5557b0c
cupokyle <kyleforsythcomedy@gmail.com>
2021-09-13 23:49:05 +0000

    Exercise 5 complete

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,9 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+puts Store.sum(:annual_revenue)
+puts "**********************************"
+puts Store.average(:annual_revenue)
+puts "**********************************"
+puts Store.where("annual_revenue > 1000000").count
